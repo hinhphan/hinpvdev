86b12483f86a31b0bd8839a2b48baf1ce24bfb7e
hinhphan <phana2311@gmail.com>
2025-07-05 23:57:57 +0700

    up

--- app/Services/PlaceholdImage/ImageGeneratorService.php
+++ app/Services/PlaceholdImage/ImageGeneratorService.php
@@ -41,14 +41,16 @@ class ImageGeneratorService extends BaseService {
         $textColor = imagecolorallocate($img, $color['r'], $color['g'], $color['b']);
 
         // Text
+        $fontPath = public_path('fonts/MSGothic.ttf');
         $text = $data['text'] ?? '';
-        $fontSize = 5;
-        $textWidth = imagefontwidth($fontSize) * strlen($text);
-        $textHeight = imagefontheight($fontSize);
+        $fontSize = $this->getMaxFontSize($text, $fontPath, $data['width'] - 20, $data['height'] - 20);
+        $box = imagettfbbox($fontSize, 0, $fontPath, $text);
+        $textWidth = abs($box[2] - $box[0]);
+        $textHeight = abs($box[5] - $box[1]);
         $x = ($data['width'] - $textWidth) / 2;
-        $y = ($data['height'] - $textHeight) / 2;
+        $y = ($data['height'] + $textHeight) / 2;
 
-        imagestring($img, $fontSize, $x, $y, $text, $textColor);
+        imagettftext($img, $fontSize, 0, $x, $y, $textColor, $fontPath, $text);
 
         // Output File
         $tmpPath = tempnam(sys_get_temp_dir(), 'img_') . '.png';
@@ -58,4 +60,17 @@ class ImageGeneratorService extends BaseService {
 
         return new File($tmpPath);
     }
+
+    protected function getMaxFontSize($text, $fontPath, $maxWidth, $maxHeight, $min = 5, $max = 100) {
+        for ($size = $min; $size <= $max; $size++) {
+            $box = imagettfbbox($size, 0, $fontPath, $text);
+            $textWidth = abs($box[2] - $box[0]);
+            $textHeight = abs($box[5] - $box[1]);
+
+            if ($textWidth > $maxWidth || $textHeight > $maxHeight) {
+                return $size - 1;
+            }
+        }
+    return $max;
+}
 }
