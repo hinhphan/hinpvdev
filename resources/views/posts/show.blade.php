6632f332fc24ecb08ce8048f52aa0d08d1c03117
hinhphan <phana2311@gmail.com>
2025-10-05 10:52:21 +0700

    feat: add new Blade views for about, 404 error, posts index, posts show, search, and tags; update routes for posts detail view

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.master>
+    <x-slot:title>
+        Post Detail
+    </x-slot:title>
+
+    
+    
+</x-layouts.master>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    return view('posts.show', ['slug' => $slug]);
+});
